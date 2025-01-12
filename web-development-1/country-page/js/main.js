d3b40391d68c0cd71f790a15a729adba5e93d040
ihawp <ihawpebay@gmail.com>
2025-01-11 22:32:03 -0600

    #hero added, 96 100 100 100 on mobile, 100 100 100 100 desktop, image size should be smaller for mobile... perhaps two versions of image?

--- web-development-1/country-page/js/main.js
+++ web-development-1/country-page/js/main.js
@@ -7,9 +7,13 @@ class MobileNav {
         document.getElementById("button-exit-mobile-nav").addEventListener('click', this.close)
     }
     open() {
+        document.querySelector('footer').style.display = 'none';
+        document.querySelector('main').style.display = 'none';
         return document.getElementById("mobile-nav").style.display = 'flex';
     }
     close() {
+        document.querySelector('footer').style.display = 'flex';
+        document.querySelector('main').style.display = 'flex';
         return document.getElementById("mobile-nav").style.display = 'none';
     }
 }
