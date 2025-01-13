a0e76dc506478c1c8ff85a8a6e650686a6f0ef9e
ihawp <ihawpebay@gmail.com>
2025-01-13 00:16:30 -0600

    eating mashed potatoes... table styled, some filled with fake data content, going to add grid, audio, newer-more-custom favicon next!

--- web-development-1/country-page/js/faq.js
+++ web-development-1/country-page/js/faq.js
@@ -9,17 +9,15 @@ class FAQ {
         this.data.map((item, index) => {
             faq.insertAdjacentHTML('beforeend', `
                 <li>
-					<details id="details-${index}" class="flex flex-col px-1">
-						<summary>
+					<details id="details-${index}" class="flex flex-col">
+						<summary class="px-1" id="summary-${index}">
 						    <h3>${item.summary}</h3>
 						</summary>
-						<div class="mt-half">
-						    <p>${item.details}</p>						
-                        </div>
+						<p class="pb-1">${item.details}</p>	
 					</details>
 				</li>
 				`);
-            document.getElementById(`details-${index}`).addEventListener('click', () => {
+                document.getElementById(`summary-${index}`).addEventListener('click', () => {
                 try {
                     document.getElementById(`details-${this.open}`).removeAttribute("open");
                 }
