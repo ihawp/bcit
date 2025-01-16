bc4691f68c8df60c4a068db71da63cca9b4edd63
ihawp <ihawpebay@gmail.com>
2025-01-16 14:04:21 -0600

    starting work on ws1-a3

---
+++ web-scripting-1/assignment3/scripts/script.js
@@ -0,0 +1,18 @@
+
+
+class Wow {
+    constructor() {
+
+    }
+}
+
+// get buttons from DOM
+const left = document.getElementById('btn-turn-clockwise');
+const right = document.getElementById('btn-turn-counter-clockwise');
+
+
+function leftClick() {
+
+}
+
+left.addEventListener('click', )
