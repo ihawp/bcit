af415395a28923529e07bb701d2a2f25be32f664
ihawp <ihawpebay@gmail.com>
2025-01-16 15:33:48 -0600

    ws1-a3 forward and backward working perfectly, now to minimize!

--- web-scripting-1/assignment3/scripts/script.js
+++ web-scripting-1/assignment3/scripts/script.js
@@ -1,5 +1,3 @@
-
-
 class HorizontalCarousel {
     constructor() {
         this.count = document.querySelectorAll('.slide img');
@@ -9,16 +7,37 @@ class HorizontalCarousel {
     init() {
         document.getElementById('btn-turn-clockwise').addEventListener('click', this.click.bind(this));
         document.getElementById('btn-turn-counter-clockwise').addEventListener('click', this.click.bind(this));
+        this.count.forEach((item, keys) => {
+           keys === 0 ? '' : item.style.display = 'none' ;
+        });
     }
     updateSlide = () => {
         this.count[this.current].style.display = 'none';
         this.current + 1 === this.count.length ? this.current = 0 : this.current += 1;
         this.count[this.current].style.display = 'block';
     }
+    removeCurrent() {
+        this.count[this.current].style.display = 'none';
+    }
+
     click(event) {
+
         console.log(this.current);
-        this.updateSlide();
-        // event.target holds id for button clicked
+
+        // remove thing on screen at time of clicking
+        this.removeCurrent();
+        console.log(this.current);
+
+        // check for which button was clicked determining increment
+        event.target.id === 'btn-turn-clockwise' ? this.current -= 1 : this.current += 1;
+
+        if (this.current === -1) {
+            this.current = this.count.length;
+        }
+        if (this.current >= this.count.length) {
+            this.current = 0
+        }
+        this.count[this.current].style.display = 'block';
     }
 }
 
