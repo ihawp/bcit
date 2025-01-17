6c0a35877303e2f1513ac5fb053e71990ebe2cf8
ihawp <ihawpebay@gmail.com>
2025-01-17 13:58:02 -0600

    close

--- web-scripting-1/assignment4/scripts/script.js
+++ web-scripting-1/assignment4/scripts/script.js
@@ -6,6 +6,24 @@
 
  */
 
+const images = {
+    black: {
+        0: 't-shirt-black-back.jpg',
+        1: 't-shirt-black-front.jpg',
+        2: 't-shirt-black-no-model.jpg'
+    },
+    red: {
+        0: 't-shirt-red-back.jpg',
+        1: 't-shirt-red-front.jpg',
+        2: 't-shirt-red-no-model.jpg'
+    },
+    grey: {
+        0: 't-shirt-grey-back.jpg',
+        1: 't-shirt-grey-front.jpg',
+        2: 't-shirt-grey-no-model.jpg'
+    },
+}
+
 const slides = document.querySelectorAll('.th');
 slides.forEach((wow) => {
     wow.addEventListener('mouseover', () => {
@@ -15,11 +33,29 @@ slides.forEach((wow) => {
 
 const colours = document.querySelectorAll('[name="shirt-color"]');
 colours.forEach((wow) => {
-    console.log(wow);
     wow.addEventListener('click', (event) => {
-        slides.forEach((wow) => {
-            console.log(wow);
-        })
+        let l = slides[0].childNodes[1].childNodes[1];
+        console.log(event);
+        let wow = l.src;
+
+        let p = wow.split('/');
+        console.log('wow', p);
+        console.log(p.length);
+        console.log(p[p.length]);
+        console.log({l});
+        switch (wow.value) {
+            case ('black'):
+
+                break;
+            case ('red'):
+                break;
+            case ('grey'):
+
+                break;
+        }
+
+        l.src = images.black[0].slice(-1, '/');
+
     })
 })
 
