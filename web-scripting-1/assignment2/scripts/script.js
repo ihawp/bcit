4926641269709d375b978b2e8ee0f86a7c8bee28
ihawp <ihawpebay@gmail.com>
2025-01-17 15:44:43 -0600

    a4 confused... need to retry, improved readability a2 ws1

--- web-scripting-1/assignment2/scripts/script.js
+++ web-scripting-1/assignment2/scripts/script.js
@@ -10,14 +10,12 @@ class Bigfoot {
     }
 
     placeBigfoot() {
-
-        const bigfoot = document.getElementById('bigfoot');
         let l = Math.random() * 100;
         let q = Math.random() * 100;
-        bigfoot.style.top = `${q}%`;
-        bigfoot.style.left = `${l}%`;
-        bigfoot.style.display = 'block';
-        bigfoot.addEventListener('click', this.foundBigfoot);
+        this.bigfoot.style.top = `${q}%`;
+        this.bigfoot.style.left = `${l}%`;
+        this.bigfoot.style.display = 'block';
+        this.bigfoot.addEventListener('click', this.foundBigfoot);
     }
 
     foundBigfoot = () => {
