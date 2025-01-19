d272b48f67ce6c889478f1628a3915fb49ff2840
ihawp <ihawpebay@gmail.com>
2025-01-18 22:16:28 -0600

    commenting

--- web-scripting-1/assignment4/scripts/script.js
+++ web-scripting-1/assignment4/scripts/script.js
@@ -10,25 +10,25 @@
 
  */
 
-// main image hover slides.
+// Main Image / Image Slide.
 const slides = document.querySelectorAll('.th a img');
 const slideContainer = document.querySelector('.slide-container');
 let current = 0;
 
-// colour selection.
+// Colour Related.
 const red = document.getElementById('t-shirt-color-red');
 const grey = document.getElementById('t-shirt-color-grey');
 const black = document.getElementById('t-shirt-color-black');
 let colourOutput = document.getElementById('selected-color-out')
 
-// size selection.
+// Size Related.
 const small = document.getElementById('t-shirt-size-small');
 const medium = document.getElementById('t-shirt-size-medium');
 const large = document.getElementById('t-shirt-size-large');
 const addToCart = document.getElementById('btn-add-to-cart');
 const selectedSize = document.getElementById('selected-size-out');
 
-// image data: src / alt.
+// Image Data
 const images = {
     't-shirt-color-black': {
         0: {
@@ -74,17 +74,46 @@ const images = {
     }
 }
 
-// Add 'click' event listeners to colour buttons
+/*
+
+    Had tried using the red, black, grey variables from way above prior to this,
+    but they simply just didn't work in the sense that they never could detect
+    a 'mouseenter' or anything of the sort.
+
+*/
+let preloadMouseEnter = document.querySelectorAll('.form-group-color div.form-item');
+
+/*
+
+    Add 'mouseenter' event listeners to colour buttons.
+
+*/
+preloadMouseEnter[1].addEventListener('mouseenter', preload, { once: true });
+preloadMouseEnter[2].addEventListener('mouseenter', preload, { once: true });
+
+/*
+
+    Add 'click' event listeners to colour buttons.
+
+*/
 red.addEventListener('click', changeColour);
 grey.addEventListener('click', changeColour);
 black.addEventListener('click', changeColour);
 
-// Add 'click' event listeners to size buttons
+/*
+
+    Add 'click' event listeners to size buttons.
+
+*/
 small.addEventListener('click', changeSize);
 medium.addEventListener('click', changeSize);
 large.addEventListener('click', changeSize);
 
-// update main image on slide 'mouseover'.
+/*
+
+    Update main image on slide image 'mouseover'.
+
+*/
 slides.forEach((item, key) => {
     item.addEventListener('mouseover', () => {
         current = key;
@@ -92,68 +121,114 @@ slides.forEach((item, key) => {
     })
 });
 
-// Function added to event listener 'click' for
-// colour selection buttons
+/*
+
+    Function added to event listener 'click' for
+    colour selection buttons.
+
+*/
 function changeColour(event) {
 
-    // Split the id to begin getting the colour
+    // Split the id to begin getting the colour.
     let l = event.target.id.split('-');
 
-    // Get last element of split string array
+    // Get last element of split string array.
     let colour = l[l.length - 1];
 
-    // Set the colour output to be formatted as first letter uppercase
+    // Set the colour output to be formatted as first letter uppercase.
     colourOutput.innerText = colour[0].toUpperCase() + colour.substring(1, colour.length);
 
-    // Set the new images using the setSlides() function
+    // Set the new images using the setSlides() function.
     setSlides(images[event.target.id]);
+
 }
 
-// Set all images when a new colour is chosen to view
+/*
+
+    Set all images when a new colour is chosen to view.
+
+    images: list of images from images array that
+            has been indexed to the proper set of 3.
+
+*/
+
 function setSlides(images) {
-    // Iterate through slides array using param info
-    // to properly update DOM data
+
+    /*
+
+        Iterate through slides array using param info.
+        to properly update DOM data.
+
+    */
     slides.forEach((item, key) => {
-        // Use key (index of loop) to get proper images sub-array
+     */
+
+        // Use key (index of loop) to get proper images sub-array.
         let indexed = images[key];
 
-        // Set a formatted link to the image
+        // Set a formatted link to the image.
         let image = 'images/' + indexed.src;
 
-        // Set the main image if the current index of type of image
-        // matches the one being viewed as the main image currently
+        /*
+
+            Set the main image if the current index of type of image
+            matches the one being viewed as the main image currently.
+
+         */
         if (key === current) {
-            // Set the image link
+
+            // Set the image link.
             slideContainer.firstElementChild.src = image;
-            // Set the alt tag
+
+            // Set the alt tag.
             slideContainer.firstElementChild.alt = indexed.alt;
         }
-        // Set the slide images (everytime)
-        // Set the image link
+
+        // Set the slide images.
+        // Set the image link.
         item.src = image;
-        // Set the alt tag
+
+        // Set the alt tag.
         item.alt = indexed.alt;
+
     });
+
 }
 
-// Change the text displayed as the current selected size
+/*
+
+    Change the text displayed as the current selected size.
+
+*/
 function changeSize(event) {
-    // Enable the "Choose A Size"/"Add To Cart" Button
+
+    // Enable the "Choose A Size"/"Add To Cart" Button.
     addToCart.removeAttribute('disabled');
 
-    // Change value of "Choose A Size"/"Add To Cart" Button
+    // Change value of "Choose A Size"/"Add To Cart" Button.
     addToCart.value = 'Add To Cart';
 
     // Split the id of the size button used to get size
     let l = event.target.id.split('-');
-    // https://www.measurethat.net/Benchmarks/Show/7476/0/char-index-vs-charat-vs-slice#:~:text=In%20modern%20JavaScript%20engines%2C%20the,that%20need%20to%20be%20modified.
-    // Get the first character of the last item in split id array
-    // (the size) use toUpperCase and input the character as the
-    // innerText of the selected size <span>
+
+    /*
+
+        https://www.measurethat.net/Benchmarks/Show/7476/0/char-index-vs-charat-vs-slice#:~:text=In%20modern%20JavaScript%20engines%2C%20the,that%20need%20to%20be%20modified.
+
+        Get the first character of the last item in split id array
+        (the size) use toUpperCase and input the character as the
+        innerText of the selected size <span>.
+
+    */
     selectedSize.innerText = l[l.length - 1][0].toUpperCase();
+
 }
 
-// Start adding of preload links based on id of colour button
+/*
+
+    Start adding of preload links based on id of colour button.
+
+*/
 function preload(event) {
     let id = event.target.firstElementChild.id;
     addLink(id, 0);
@@ -161,22 +236,23 @@ function preload(event) {
     addLink(id, 2);
 }
 
-// Create <link rel="preload" as="image" href="images/...">
-// in head of DOM to preload the next colour images before the user
-// presses the colour button
+/*
+
+    Create <link rel="preload" as="image" href="images/...">
+    in head of DOM to preload the next colour images before the user
+    presses the colour button.
+
+    id: index of group of images to be preloaded.
+
+    index: the index of the image to be preloaded
+           at this time.
+
+*/
 function addLink(id, index) {
     // https://www.w3schools.com/jsref/met_document_createelement.asp
-    // Forgot createElement!
     let l = document.createElement("link");
     l.rel = 'preload';
     l.as = 'image';
     l.href = `images/${images[id][index].src}`;
     document.head.appendChild(l);
 }
-
-// Had tried using the red, black, grey variables from way above prior to this
-// but they simply just didn't work in the sense that they never could detect
-// a 'mouseover'
-let bananaPhone = document.querySelectorAll('.form-group-color div.form-item');
-bananaPhone[1].addEventListener('mouseenter', preload, { once: true });
-bananaPhone[2].addEventListener('mouseenter', preload, { once: true });
