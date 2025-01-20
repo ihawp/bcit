af9835b1e634b0cc2fb1159ae8b78ff41b945ce3
ihawp <ihawpebay@gmail.com>
2025-01-20 15:55:34 -0600

    updated header to have so much less code overall, looks much better now too! opted for the same page mobile nav rather then a full page one, create a way nicer feel of the site, very happy with this version! wd1-country-page

--- web-development-1/country-page/js/main.js
+++ web-development-1/country-page/js/main.js
@@ -4,17 +4,17 @@ class MobileNav {
     }
     init() {
         document.getElementById("button-mobile-nav").addEventListener('click', this.open);
-        document.getElementById("button-exit-mobile-nav").addEventListener('click', this.close)
+        document.getElementById("button-exit-mobile-nav").addEventListener('click', this.close);
     }
     open() {
-        document.querySelector('footer').style.display = 'none';
-        document.querySelector('main').style.display = 'none';
-        return document.getElementById("mobile-nav").style.display = 'flex';
+        document.getElementById('button-exit-mobile-nav').removeAttribute('hidden');
+        document.getElementById('button-mobile-nav').setAttribute('hidden', 'hidden');
+        document.getElementById("navigation").classList.remove('display-sm');
     }
     close() {
-        document.querySelector('footer').style.display = 'flex';
-        document.querySelector('main').style.display = 'flex';
-        return document.getElementById("mobile-nav").style.display = 'none';
+        document.getElementById('button-exit-mobile-nav').setAttribute('hidden', 'hidden');
+        document.getElementById('button-mobile-nav').removeAttribute('hidden');
+        document.getElementById("navigation").classList.add('display-sm');
     }
 }
 
