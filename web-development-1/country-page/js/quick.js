4e60d3c87c08629d16953643ff9e68e54d895c7d
ihawp <ihawpebay@gmail.com>
2025-01-20 12:01:16 -0600

    reformatting of index.html, added meta tags for page indexing...

--- web-development-1/country-page/js/quick.js
+++ web-development-1/country-page/js/quick.js
@@ -1,1 +1,4 @@
+/*
+    MIGHT REMOVE THIS AND REFIGURE LOGIC FOR IMAGE LOADING ONCE PAGES FILLED
+*/
 document.getElementById("hero-image").src = `media/bg${window.innerWidth < 640 ? '-mobile' : ''}.webp`;
