0d6d07f8db71b6a9c8407fe55128edd3d1cba566
ihawp <ihawpebay@gmail.com>
2025-01-19 21:37:40 -0600

    working on country-page for wd1-country-page added ability for multiple lines of <p> in details FAQ sections when init with data

--- web-development-1/country-page/js/faq.js
+++ web-development-1/country-page/js/faq.js
@@ -1,29 +1,27 @@
 class FAQ {
     constructor(data) {
+        this.faq = document.getElementById('faq');
         this.open = 0;
         this.data = data;
         this.init();
     }
     init() {
-        const faq = document.getElementById('faq');
         this.data.map((item, index) => {
-            faq.insertAdjacentHTML('beforeend', `
+            let items = '';
+            item.details.map(item => items += `<p class="pb-1">${item}</p>`);
+            this.faq.insertAdjacentHTML('beforeend', `
                 <li>
 					<details id="details-${index}" class="flex-col">
 						<summary class="px-1" id="summary-${index}">
 						    <h3>${item.summary}</h3>
 						</summary>
-						<p class="pb-1">${item.details}</p>	
+						${items}
 					</details>
 				</li>
-				`);
-                document.getElementById(`summary-${index}`).addEventListener('click', () => {
-                try {
+			`);
+            document.getElementById(`summary-${index}`).addEventListener('click', () => {
                     document.getElementById(`details-${this.open}`).removeAttribute("open");
-                }
-                finally {
                     this.open = index;
-                }
             });
         })
     }
