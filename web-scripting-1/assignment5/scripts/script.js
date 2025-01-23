588bb1d89254b296944ea08ab9ad3d3e4846fef6
ihawp <ihawpebay@gmail.com>
2025-01-23 11:29:30 -0600

    ws1-a5 just need to add the button functionality on final screen!

--- web-scripting-1/assignment5/scripts/script.js
+++ web-scripting-1/assignment5/scripts/script.js
@@ -14,12 +14,17 @@ const images = document.querySelectorAll('.cat-image-container');
 const imagesContainer = document.querySelector('.cat-images-container');
 const nameCreator = document.querySelector('.cat-name-creator');
 const gameboardContainer = document.querySelector('.gameboard-container');
+const catImageOutput = document.querySelector('.cat-image-output');
 const form = document.querySelector('form');
 images.forEach((item) => {
     item.addEventListener('click', doThing);
 });
 
+let current = undefined;
+
 function doThing(event) {
+    // beautiful!
+    catImageOutput.src = event.target.src;
     console.log(event.target.src); // get this value to event listener
     imagesContainer.style.display = 'none';
     nameCreator.style.display = 'block';
