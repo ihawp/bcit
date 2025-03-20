a88a79fdd209b0ef683ec2a0874a38866eea91d1
ihawp <ihawpebay@gmail.com>
2025-03-20 11:52:43 -0600

    web-scripting-2 assignment-2 completed

---
+++ web-scripting-2/assignment-2/data.php
@@ -0,0 +1,17 @@
+<?php
+/**********************************
+do not add to or alter the variables declared here
+use these variables in your own script by loading
+this page into your .php page
+
+Note: feel free to change the values assigned to the user's 
+birthdate so that you can test for various outcomes
+***********************************/
+$birthMonth		= "March";   	//month user was born
+$birthDay		= 21;     		//day of month user was born
+
+//this string message will be used to try out some string formatting functions
+$message		= "ALL UPPERCASE STRING TO BE FORMATTED";
+
+
+?>
