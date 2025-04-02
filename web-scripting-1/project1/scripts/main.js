f592d903246b07cfa1f8371bd8498c756f946e4e
ihawp <ihawpebay@gmail.com>
2025-04-02 16:18:55 -0600

    added canvas implementation because it is probably more sound than creating 500 spans inside a div!

--- web-scripting-1/project1/scripts/main.js
+++ web-scripting-1/project1/scripts/main.js
@@ -2,13 +2,21 @@
 
 import { Game } from './game.js';
 import Panel from './particle.js';
+import { ParticleBackground } from './particle.js';
 
 export const canvas = document.getElementById('canvas');
 export const context = canvas.getContext("2d");
-export const framerate = 33.03030;
 
-const panel = new Panel();
-panel.init();
+// 30 fps: 33.3333333333 (intended fps)
+// 60 fps: 16.6666666667 (speeds game up)
+export const framerate = 33.3333333333;
 
+// Particle Background Instance (DOM)
+// new Panel().init();
+
+// Particle Background Instance (Canvas)
+new ParticleBackground().init();
+
+// Game Instance / Run Game
 const game = new Game();
 game.init();
--- web-scripting-1/project1/scripts/particle.js
+++ web-scripting-1/project1/scripts/particle.js
@@ -1,15 +1,26 @@
-// For particle background?
+/*
+    Two Implementations
+
+    1. DOM: Uses <div> and <span> elements.
+
+    2. Canvas: Particles are drawn to <canvas>. 
+    
+    All particle instances are stored in array in ( instanceName.particles ).
+
+*/
 
 import { randomNumberInRange } from './functions.js';
 
+// DOM Version
 export default function Panel() {
     this.panel = document.createElement('div');
+    this.panel.id = ('panel');
     this.style = this.panel.style;
     this.style.position = 'absolute';
     this.style.top = '0';
     this.style.left = '0';
     this.style.width = '100vw';
-    this.style.height = '100vh';
+    this.style.height = '100vw';
     this.style.zIndex = '-1';
 
     this.particles = [];
@@ -37,4 +48,43 @@ export default function Panel() {
 
         }
     }
+}
+
+// Move in circle around radius from center of screen?
+// Update position on each draw.
+
+// Canvas Version
+export function ParticleBackground() {
+
+    this.canvas = document.createElement('canvas');
+    this.particles = [];
+    this.context = this.canvas.getContext('2d');
+    this.canvas.id = 'particle-background';
+    this.style = this.canvas.style;
+    this.style.position = 'absolute';
+    this.style.top = '0';
+    this.style.left = '0';
+    this.canvas.setAttribute('width', `${window.innerWidth}`);
+    this.canvas.setAttribute('height', `${window.innerHeight}`);
+    this.style.zIndex = '-1';
+
+    this.init = function() {
+        document.body.insertAdjacentElement('afterbegin', this.canvas);
+
+        this.context.fillStyle = '#fff';
+        for (let i = 0; i < 500; i++) {
+
+            let particle = new Particle();
+
+            this.context.fillRect(particle.x, particle.y, 1, 1);
+
+            this.particles.push(particle);
+
+        }
+    }
+}
+
+function Particle() {
+    this.x = randomNumberInRange(0, window.innerWidth);
+    this.y = randomNumberInRange(0, window.innerHeight);
 }
