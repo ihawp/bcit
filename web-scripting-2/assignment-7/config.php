8ce54eb372ee86f1f260e07652aa66c5538bb7f6
ihawp <ihawpebay@gmail.com>
2025-04-08 14:26:02 -0600

    web-scripting-2 assignment-7

---
+++ web-scripting-2/assignment-7/config.php
@@ -0,0 +1,6 @@
+<?php
+//define the default location of the available stylesheets
+$stylesheetDirectory    = "css/";
+//choose a default in case the user does not have a preference
+$defaultStylesheet      = "style.css";
+?>
