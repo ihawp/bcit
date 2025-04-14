48725b31b71d062c084bbe3b50765ca18a12186d
ihawp <ihawpebay@gmail.com>
2025-04-14 13:13:43 -0600

    web-scripting-3 assignment-2

--- web-scripting-3/assignment-2/src/App.jsx
+++ web-scripting-3/assignment-2/src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import quotes from './quotes.js';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Quote from './Quote.jsx';
 import { randomIntWithMax } from './functions.js';
-import './App.css'
+import './App.css';
 
 function App() {
 
-  const [thing, setThing] = useState(-1);
+  const [current, setCurrent] = useState(-1);
 
   const changeQuote = () => {
     let q = randomIntWithMax(quotes.length);
-    q === thing && quotes[q - 1] ? q-- : q++;
-    q === thing && quotes[q + 1] ? q++ : q--;
-    setThing(q);
+    q === current && quotes[q - 1] ? q-- : q++;
+    q === current && quotes[q + 1] ? q++ : q--;
+    setCurrent(q);
   }
 
   return <>
     {Header()}
-    <div className="quote-outer">{thing === -1 ? 'Click to get a quote!' : Quote(quotes[thing])}</div>
-    <button onClick={changeQuote}>{thing > -1 ? 'Get Another Quote' : 'Get Quote'}</button>
+    <div className="quote-outer">{current === -1 ? 'Click to get a quote!' : Quote(quotes[current])}</div>
+    <button onClick={changeQuote}>{current > -1 ? 'Get Another Quote' : 'Get Quote'}</button>
     {Footer()}
   </>;
 }
