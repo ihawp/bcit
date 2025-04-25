e531442e00111ef76ea60bb66cfd10fd2e749b1d
ihawp <ihawpebay@gmail.com>
2025-04-24 19:28:50 -0600

    some general styling

--- web-scripting-2/project-1/index.php
+++ web-scripting-2/project-1/index.php
@@ -20,35 +20,6 @@ include_once 'html/header.html';
     <p>Use the navigation bar to find the website content!</p>
 </section>
 
-<section>
-    <h1>TODO:</h1>
-    <ul class="pl-2 disc">
-        <li>
-            <p>- Error handling for all pages that send errors.</p>
-            <ul class="pl-2 disc">
-                <li>
-                    <p>Login</p>
-                </li>
-                <li>
-                    <p>Register</p>
-                </li>
-                <li>
-                    <p>allStudents (from updateRecord / deleteConfirm)</p>
-                </li>
-                <li>
-                    <p>deleteConfirm (from commitDelete)</p>
-                </li>
-                <li>
-                    <p>updateRecord (from submitUpdateRecord) </p>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <p>Make sure DB connection is not initialized before it is known that it is 100% required.</p>
-        </li>
-    </ul>
-</section>
-
 <?php
 
 include_once 'html/footer.html';
