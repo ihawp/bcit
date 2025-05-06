5a76efccbbb9dd8b2c3eea5e7f36ce8f8be6d197
ihawp <ihawpebay@gmail.com>
2025-05-05 23:44:37 -0600

    just need to finish styling for individual page

--- web-scripting-3/project-1/src/pages/About.jsx
+++ web-scripting-3/project-1/src/pages/About.jsx
@@ -8,8 +8,8 @@ export default function About() {
             <p>At Movie DB, you'll find an extensive collection of movie and TV show listings, complete with cast and crew information, plot summaries, release dates, trailers, ratings, and user reviews. We also provide detailed profiles of actors, directors, writers, and other industry professionals, allowing you to explore their biographies, filmographies, and award histories in one place.</p>
             <p>Our platform encourages community engagement by allowing users to rate titles, write reviews, and share recommendations. You can create personalized watchlists to keep track of what you've seen and what you plan to watch next. Plus, we keep you informed about trending content and upcoming releases across theaters and streaming platforms.</p>
             <p>Our mission is to connect people through the shared love of storytelling. We believe that every film and show deserves to be discovered and celebrated, and we're committed to providing a dynamic and evolving resource for everyone who loves entertainment. Whether you're researching, browsing, or planning your next movie night, Movie DB is here to enrich your experience and spark your passion for film and TV.</p>
-            <p>All movie data on this website was obtained from The Movie Database (TMDB, tmdb.org).</p>
-            <img src="tmdb-logo.svg" alt="The Movie Database (TMDB, tmdb.org) Logo" draggable="false" />
+            <p>All movie data on this website was obtained from The Movie Database. You can visit their website <a className="underline" href="https://www.themoviedb.org" title="TMDB Link" aria-label="Link to The Movie Database dot org">here</a>.</p>
+            <img src="tmdb-logo.svg" alt="The Movie Database (TMDB, themoviedb.org) Logo" draggable="false" />
         </section>
     </main>
 }
